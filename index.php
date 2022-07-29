9254a9cc40a99746362d3d03f2f67d0d487e4e64
hidayatTM <muhtar.mis@gmail.com>
2022-07-29 15:11:46 +0700

    manambahkan list pada index.php

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio corporis quaerat unde velit voluptatem. Maiores quos, sequi excepturi qui aliquam provident dolor, quas libero in aut doloribus et dolorum unde. Magnam, voluptates dignissimos ipsum sequi ab molestiae sit, sapiente similique, error consequuntur nisi quod eligendi. Voluptates, magnam suscipit. Inventore nemo magni ratione error tenetur. Assumenda, nesciunt ratione asperiores quae voluptatum doloribus facere! Quos enim illo accusantium, autem quisquam blanditiis voluptatem. Id iste ducimus totam ullam accusamus, amet quasi cumque fugit fuga labore nobis rem adipisci eius ex repellendus. Doloremque tempora error temporibus sit ipsa neque ab exercitationem sequi non! Totam?</p>
+
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
